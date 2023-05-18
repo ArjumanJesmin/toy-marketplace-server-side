158d8a9f43ef63e3639445c0f93b038520e64dd5
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-18 10:51:57 +0600

    mongodb create data done and display all data in localhost

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(express.json())
 
 // ------------------------------------------------------------------------
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.dkozdag.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkozdag.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,12 +29,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('babyDoll').collection('dolls')
+
+     //services
+     app.get('/toys', async (req, res) => {
+      const cursor = toysCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -43,10 +51,10 @@ run().catch(console.dir);
 // ------------------------------------------------------------------------
 
 app.get('/', (req, res) => {
-    res.send('baby toy zone is start')
+  res.send('baby toy zone is start')
 })
 
 
 app.listen(port, () => {
-    console.log(`Baby Toy Server is running on port: ${port}`)
+  console.log(`Baby Toy Server is running on port: ${port}`)
 })
