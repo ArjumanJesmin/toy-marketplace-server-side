d664e0bfdb6b25541cbb69f2a7065ceb26b19364
ArjumanJesmin <arjumanjesmin221186@gmail.com>
2023-05-20 12:17:06 +0600

    deleted successfully

--- index.js
+++ index.js
@@ -122,6 +122,14 @@ async function run() {
       res.send(result)
     })
 
+    // data delete
+    app.delete('/someToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await dollCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
